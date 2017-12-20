dfde9c86c7131e719d1f34dc1fd815287f797895
balamut108 <balamut108@gmail.com>
2017-12-20 13:57:22 +0300

    Add sample local_settings

---
+++ sample_local_settings.py
@@ -0,0 +1,16 @@
+ETH_PWD = ''
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+    },
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
