27fdd349ff39d1acb09f6d854b32efc3ff27e461
balamut108 <balamut108@gmail.com>
2017-12-20 13:52:05 +0300

    Add local_settings

---
+++ local_settings.py
@@ -0,0 +1,16 @@
+ETH_PWD = 'balamutina108'
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fixthedoc',
+        'USER': 'fixthedoc',
+        'PASSWORD': 'fixthedoc',
+    },
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '_r@^_%retrpb8in$e+ovy5wf!^x+w3&lowj04z793=8k1w!(w!'
