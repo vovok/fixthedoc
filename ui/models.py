29656505ef7d82fe380b70a99107b16ae214ebb3
balamut108 <balamut108@gmail.com>
2017-12-21 16:45:13 +0300

    Rollback

--- ui/models.py
+++ ui/models.py
@@ -33,7 +33,7 @@ class FormUploadFiles(ModelForm):
         if file_src:
             file = file_src.file.read()
             cleaned_data['file_hash'] = hashlib.sha1(file).hexdigest()
-            if UploadFiles.objects.filter(file_hash=cleaned_data['file_hash']) or check_hash_from_bch(cleaned_data['file_hash']):
+            if UploadFiles.objects.filter(file_hash=cleaned_data['file_hash']): #or check_hash_from_bch(cleaned_data['file_hash'])
                 self.add_error('file_hash', 'Файл с данным хешем был загружен ранее.')
             if file_src.size > 1048576:
                 self.add_error('file_src', 'Файл слишком большой.')
